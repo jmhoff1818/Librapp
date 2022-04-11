4f9998e6aed182bb591952eeaeb1cada13f05777
Annie <dimaam@miamioh.edu>
2022-04-11 15:55:05 -0400

    more database

--- connect.php
+++ connect.php
@@ -3,5 +3,16 @@ $email=$_POST['email'];
 $password=$_POST['password'];
 
 //connect to database
-
+$conn= new mysqli('localhost','root','','librapp');
+if($conn->connect_error){
+  die('Connection Failed :' .$conn->connection_error);
+}
+else{
+  $stmt=$conn->prepare("Insert into registration(email,password) values(?,?)");
+  $stmt->bind_param("ss", $email,$password);
+  $stmt->execute();
+  echo("registration succesfull");
+  $stmt->close();
+  $conn->close();
+}
 ?>
